ccc165cde202ff482e40a348aca3a044d05e378d
antonio <antoniopa@ipvc.pt>
2019-05-27 04:50:43 -0700

    Initial Commit

---
+++ MyPillsIOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyPillsIOS
+//
+//  Created by User on 27/05/2019.
+//  Copyright © 2019 ipvc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
