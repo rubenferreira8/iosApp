a05ff0d94b47e2c527f852e07fc375399c77dffe
antonio <antoniopa@ipvc.pt>
2019-05-27 20:44:25 -0700

    .

--- MyPillsIOS/ViewController.swift
+++ MyPillsIOS/ViewController.swift
@@ -7,14 +7,81 @@
 //
 
 import UIKit
+import Firebase
+import FirebaseAuth
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var signInSelector: UISegmentedControl!
+    
+    @IBOutlet weak var signInLabel: UILabel!
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    var isSignIn:Bool = true
+    
+    @IBOutlet weak var signInButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func signInSelectorChanged(_ sender: UISegmentedControl) {
+        // Flip the Boolean
+        isSignIn = !isSignIn
+        
+        // check the bool and set the button and labels
+        if isSignIn {
+            signInLabel.text = "Sign In"
+            signInButton.setTitle("Sign In", for: .normal)
+        } else  {
+        signInLabel.text = "Register"
+            signInButton.setTitle("Register", for : .normal)
+        }
+    }
+    
+    @IBAction func signInButtonTapped(_ sender: UIButton) {
+        // Todo : do some form validation on the email and password
+        if let email = emailTextField.text, let pass = passwordTextField.text {
+            // Check if it's sign in or register
+            if isSignIn  {
+            // Sign in the user with Firebase
+                Auth.auth().signIn(withEmail: email, password: pass, completion:   { (user, error) in
+                    
+                    // Check that user isn't nil
+                    if let u = user {
+                        // user is found go to home screen
+                        self.performSegue(withIdentifier: "goToHome", sender: self)
+                    }
+                    else  {
+                        // Error:check error and show message
+                        print("Parece que nao ha net")
+                        }
+                    })
+                
+            } else  {
+            // Register the user on Firebase
+                Auth.auth().createUser(withEmail: email, password: pass, completion:{ (user, error) in
+                    
+                    // Check that user isn't nil
+                    if let u = user {
+                        // user is found go to home screen
+                        self.performSegue(withIdentifier: "goToHome", sender: self)
+                    }
+                    else {
+                        // Error: check error and show message
+                    }
+            })
+            }}}
+            
+    
+    
+        
+        override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+            emailTextField.resignFirstResponder()
+            passwordTextField.resignFirstResponder()
+        }
 }
 
